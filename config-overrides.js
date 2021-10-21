9b2e55f75a7459d57f8d9d6df06198ff039a9ab1
卞刘柱 <ext.bianliuzhu@jd.com>
2021-10-21 09:53:06 +0800

    feat: 合并代码

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Gleason
  * @Date: 2021-09-24 11:32:44
- * @LastEditTime: 2021-10-19 14:42:13
+ * @LastEditTime: 2021-10-21 09:52:57
  * @Description: webpack 配置(覆盖)
  */
 
@@ -21,10 +21,6 @@ const {
 	HOST_DEVICE, // 设备
 	HOST_SERVICE, // 服务
 	HOST_ORDER, // 订单
-	HOST_UE, // UE
-	HOST_OLDFORNEWSERVICE, // 以旧换新
-	HOST_QUICKORDER, // 一键下单
-	HOST_QUICKORDERQ2, // 一键下单二期
 	HOST_WORKER, // 工人认证
 	HOST_CHARGE, // 京工巧匠小程序-收费线上化
 	REACT_APP_ENV, // 环境标识
@@ -123,46 +119,6 @@ module.exports = {
 					"^/service": "",
 				},
 			},
-			"/ue": {
-				secure: false,
-				ws: false,
-				// target: "http:" + HOST.ue,
-				target: "http:" + HOST_UE,
-				changeOrigin: true,
-				pathRewrite: {
-					"^/ue": "",
-				},
-			},
-			"/oldfornewservice": {
-				secure: false,
-				ws: false,
-				// target: "http:" + HOST.oldfornewservice,
-				target: "http:" + HOST_OLDFORNEWSERVICE,
-				changeOrigin: true,
-				pathRewrite: {
-					"^/oldfornewservice": "",
-				},
-			},
-			"/quickorder": {
-				secure: false,
-				ws: false,
-				// target: "http:" + HOST.quickorder,
-				target: "http:" + HOST_QUICKORDER,
-				changeOrigin: true,
-				pathRewrite: {
-					"^/quickorder": "",
-				},
-			},
-			"/quickorderQ2": {
-				secure: false,
-				ws: false,
-				// target: "http:" + HOST.quickorderQ2,
-				target: "http:" + HOST_QUICKORDERQ2,
-				changeOrigin: true,
-				pathRewrite: {
-					"^/quickorderQ2": "",
-				},
-			},
 			"/worker": {
 				secure: false,
 				ws: false,
@@ -171,16 +127,6 @@ module.exports = {
 				changeOrigin: true,
 				pathRewrite: {
 					"^/worker": "",
-				},
-			},
-			"/charge": {
-				secure: false,
-				ws: false,
-				// target: "http:" + HOST.charge,
-				target: "http:" + HOST_CHARGE,
-				changeOrigin: true,
-				pathRewrite: {
-					"^/charge": "",
 				},
 			},
 			[`/${REACT_APP_ENV}`]: {
