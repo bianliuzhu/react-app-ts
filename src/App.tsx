b7cd08629c78f49936c464cbd20480ed5beedfbd
卞刘柱 <ext.bianliuzhu@jd.com>
2021-10-29 17:19:50 +0800

    feat: 投票功能测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: Gleason
  * @Date: 2021-09-14 16:18:32
- * @LastEditTime: 2021-10-29 14:30:40
+ * @LastEditTime: 2021-10-29 17:04:38
  * @Description:
  */
 
@@ -10,21 +10,22 @@ import { login, vote } from "api";
 export default function App() {
 	// const aaa = useSelector((state: any) => state.counter);
 
-	const loginHandle = async () => {
-		const res = await login({
-			code: "079S6nFa1nz31C0gEjHa11Q6A40S6nFx",
-			"user-info": JSON.stringify({
-				nickName: "卞刘著",
-				gender: 0,
-				language: "zh_CN",
-				city: "",
-				province: "",
-				country: "",
-				avatarUrl:
-					"https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKd0LPDu217v67zib3C8HZCS79YqnE7phVic73mwYvlpiazwsLS3rdxgCsqalfy3Uaicia8CNu2GJeWDVg/132",
-			}),
-		});
-		console.log(res);
+	const loginHandle = () => {
+		setInterval(async () => {
+			const res = await login({
+				code: "32323423424324324242",
+				"user-info": JSON.stringify({
+					nickName: "qwerr",
+					gender: 0,
+					language: "zh_CN",
+					city: "",
+					province: "",
+					country: "",
+					avatarUrl: "qwerqwer",
+				}),
+			});
+			console.log(res);
+		}, 1000);
 	};
 	const voteHandle = async () => {
 		const res = await vote({ type: 1, sign_id: 314848 });
--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@
  * @Author: Gleason
  * @Date: 2021-04-13 16:56:39
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-10-28 17:44:16
+ * @LastEditTime: 2021-10-29 17:19:39
  */
 import dio from "./dio";
 /**
