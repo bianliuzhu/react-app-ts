af790d4251716d3ded48e05e34ea9b7eee20efba
卞刘柱 <ext.bianliuzhu@jd.com>
2021-11-24 16:36:02 +0800

    feat: redux配置typescript

--- src/store/reducer/counter.ts
+++ src/store/reducer/counter.ts
@@ -5,7 +5,8 @@
  * @LastEditors: Gleason
  * @Description: 计数store
  */
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from '../index';
 
 const initialState: initState = {
 	value: 0,
@@ -14,20 +15,23 @@ const initialState: initState = {
 
 export const counterSlice = createSlice({
 	name: 'counter',
-	initialState,
+	initialState, // `createSlice` 将从 `initialState` 参数推断状态类型
 	reducers: {
-		increment: (state: initState) => {
+		increment: (state) => {
 			state.value += 1;
 		},
-		decrement: (state: initState) => {
+		decrement: (state) => {
 			state.value -= 1;
 		},
-		incrementByAmount: (state: initState, action) => {
+		// 使用 PayloadAction 类型声明 `action.payload` 的内容
+		incrementByAmount: (state, action: PayloadAction<number>) => {
 			state.value += action.payload;
 		},
 	},
 });
-
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// 其他代码如选择器可以使用导入的`RootState`类型
+export const selectCount = (state: RootState) => state.counter.value;
 
 export default counterSlice.reducer;
